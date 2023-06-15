dffae1f6be692ae8235fbf662e6c0c02c4cbaf78
oriole crumbley <peakoneainters@gmail.com>
2023-06-15 10:44:40 -0600

    sembraschool commit

--- src/SembraSchool.tsx
+++ src/SembraSchool.tsx
@@ -1,4 +1,4 @@
-import { HStack, Select, SimpleGrid, Card, CardBody, Text } from "@chakra-ui/react";
+import { Alert, AlertIcon, InputGroup, InputLeftAddon, Center, Button, Stack, Divider, Input, Box, Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon, HStack, Select, SimpleGrid, Card, CardBody, Text } from "@chakra-ui/react";
 import "./App.css";
 import Header from './Header'
 import NavBar from './NavBar'
@@ -17,38 +17,86 @@ function SembraSchool() {
         <Text align="right">Logout</Text>
         <Text>SembraSchool Dashboard</Text>
         <HStack>
-          <Card>
-            <CardBody>
-              <NavBar />
-            </CardBody>
-          </Card>
-          <Card>
-            <CardBody>
-              <Select
-                padding="1"
-                bg="blue"
-                borderColor="white"
-                color="white"
-                placeholder="Check User Permissions"
-              />
+          <NavBar />
+        <Accordion allowToggle>
+  <AccordionItem>
+    <h2>
+      <AccordionButton>
+        <Box as="span" flex='1' textAlign='left'>
+          Check User Permissions
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+      <Stack>
+      <HStack><p>Username:</p>
+    
+    <Input placeholder='sss10137' />
+    </HStack>
+    <HStack>
+    <p>Permissions:</p>
+    <Select placeholder='Select option'>
+  <option value='Caregiver'>Option 1</option>
+  <option value='Admin'>Option 2</option>
+  <option value='Owner'>Option 3</option>
+</Select>
+    </HStack>
+    
+    </Stack>
+    <Stack spacing={3}>
+  <Alert status='error'>
+    <AlertIcon />
+    There was an error processing your request
+  </Alert>
 
-              <Select
-                padding="1"
-                bg="blue"
-                borderColor="white"
-                color="white"
-                placeholder="Change User Permissions"
-              />
-              <Select
-                padding="1"
-                bg="blue"
-                borderColor="white"
-                color="white"
-                placeholder="Change User Group"
-              />
-            </CardBody>
-          </Card>
-        </HStack>
+  <Alert status='success'>
+    <AlertIcon />
+    Success - sss100137
+  </Alert>
+
+</Stack>
+    
+    <Button colorScheme='blue'>Search</Button>
+
+    </AccordionPanel>
+  </AccordionItem>
+
+  <AccordionItem>
+    <h2>
+      <AccordionButton>
+        <Box as="span" flex='1' textAlign='left'>
+          Possible Duplicate Box
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </AccordionPanel>
+  </AccordionItem>
+
+  <AccordionItem>
+    <h2>
+      <AccordionButton>
+        <Box as="span" flex='1' textAlign='left'>
+          Check User Group
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </AccordionPanel>
+  </AccordionItem>
+</Accordion>
+</HStack>
       </SimpleGrid>
     </Card>
   );
